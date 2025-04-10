6ae8b41276f1683e7213873054d93ef27db669a4
Maranhão Azevedo <azevedev@gmail.com>
2025-04-09 23:19:29 -0300

    fixing

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,8 @@
       </transition>
     </router-view>
   <div id="nav">
-    <div style="min-height: 30px; text-align:center; z-index: 9999; font-size: 10px; width: 100%;">
-      <p v-if="currentRouteName == 'Home'" style="z-index: 9999;">Por favor confirmar presença até dia 26 de outubro de 2022.</p>
-      <p v-if="currentRouteName == 'Home'" style="z-index: 9999;">As senhas só serão entregues após a confirmação da presença.</p>
+    <div style="min-height: 40px; text-align:center; z-index: 9999; font-size: 10px; width: 100%;">
+      <p v-if="currentRouteName == 'Home'" style="z-index: 9999;">Por favor confirmar presença até dia 26 de Abril de 2025.</p>
     </div>
     <div class="links" style="margin-bottom: 20px;">
       <router-link to="/" v-if="currentRouteName !== 'Home'">Página Inicial</router-link>
